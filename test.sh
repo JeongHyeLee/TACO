90dae99fc2fd2c99be825b21cd2a1cfbabb5a40c
JeongHyeLee <40881134+JeongHyeLee@users.noreply.github.com>
2018-08-08 10:09:20 +0900

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -ex
+mkdif test
+
+echo """[enter the hostname that you want to use as master and worker node respectively]
+how many nodes you want?"""
+
+read number
+
+for i in number
+do 
+  echo "hostname:"
+  read hostname
+  name=$(echo $hostname | awk '{print $1}') >/test/test.txt
+  ipad=$(echo $hostname | sed 's/ip=//' | awk '{print $2}') >>/test/test.txt
+done 
+
