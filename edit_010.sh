b0f6775ec6a9fc2d50908c00644385d98947e856
JeongHyeLee <40881134+JeongHyeLee@users.noreply.github.com>
2018-08-10 15:57:49 +0900

    Update edit_010.sh

--- edit_010.sh
+++ edit_010.sh
@@ -26,7 +26,7 @@ if [ $OS_DISTRO == Red ]; then
     setenforce 0
 elif [ $OS_DISTRO == CentOS ]; then
     yum update -y
-    yum install -y epel-release
+    #yum install -y epel-release
     yum install -y yum-utils python-pip python-devel
     yum groupinstall -y 'development tools'
     yum -y install ceph-common git jq nmap bridge-utils net-tools
@@ -36,18 +36,17 @@ elif [ $OS_DISTRO == CentOS ]; then
     # TODO Don't leave firewall disabled permanently, use only for TACO-AIO
     systemctl disable firewalld
 elif [ $OS_DISTRO == Ubuntu ]; then
-    apt-get update
-    apt-get -y upgrade
-    apt install -y python python-pip
-    apt install -y ceph-common git jq nmap bridge-utils ipcalc
+    sudo apt-get update
+    sudo apt-get -y upgrade
+    sudo apt install -y python python-pip
+    sudo apt install -y ceph-common git jq nmap bridge-utils ipcalc
 else
     echo "This Linux Distribution is NOT supported"
 fi
-pip install --upgrade pip==9.0.3
-pip install 'pyOpenSSL==16.2.0'
-pip install 'python-openstackclient'
+sudo pip install --upgrade pip==9.0.3
+sudo pip install 'pyOpenSSL==16.2.0'
+sudo pip install 'python-openstackclient'
 
-swapoff -a
 sed -i '/swap/s/^/#/g' /etc/fstab
 modprobe rbd
-ssh-keygen -b 2048 -t rsa -f ~/.ssh/id_rsa -q -N ""
+
